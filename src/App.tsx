9e2f0da383cb45edf1dd8171420991d6a4e29535
maarten <vercammen.maarten.2000@gmail.com>
2022-06-28 14:50:03 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from './components/header/header';
-import UserOverview from './components/user/overview';
-import { AxiosResponse } from 'axios';
-import { user } from './types';
-import UserService from './services/userService';
-import AddUser from './components/user/adduser';
 
 function App() {
     return (
         <>
-            <Header/>
             <main className="container mt-5">
                 <Routes>
-                    <Route path="/" element={<UserOverview/>} />
-                    <Route path="/user/add" element={<AddUser/>} />
+                    <Route path="/" element={<p>Test</p>} />
                 </Routes>
             </main>
         </>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,38 +1,13 @@
 export interface user {
-    firstname: string;
-    lastname: string;
-    birthdate: Date;
-    email: string;
-    phone: string;
-    street: string;
-    number: number;
-    postalcode: number;
-    city: string;
-    password: string;
-    typename: string;
-    member: string;
-    leidingover: string;
-    duty: string;
-}
-
-export enum usertype {
-    'Lid',
-    'Leiding',
-    'Ouder',
-}
-
-export enum group {
-    'Rubskes',
-    'Leeuwkes',
-    'Jongknapen',
-    'Knapen',
-    'Jonghernieuwers',
-    'Hernieuwer',
-    'Leiding',
-    '',
-}
-
-export enum duty {
-    'Bonds',
-    '',
-}
+    id: number;
+    voornaam: string;
+    achternaam: string;
+    geboortedatum: Date;
+    straat: string;
+    huisnummer: number;
+    postcode: number;
+    plaats: string;
+    telefoonnummer: string;
+    usertype: string;
+  }
+  
