fa6825a2332efab6363d77336847bd136d9fd3af
emvee2000 <34810297+emvee2000@users.noreply.github.com>
2022-09-05 11:32:57 +0200

    navbar

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 const NavBar = () => {
   return (
     <nav>
-        <ul>
-            <li><a href='#start'>Home</a></li>
-            <li><a href='#info'>Info</a></li>
-            <li><a href='#braggel'>Braggel</a></li>
-            <li><a href='#News'>News</a></li>
-            <li><a href='#Leeftijden'>Leeftijden</a></li>
-            <li><a href='#Contact'>Contact</a></li>
-        </ul>
+      <a href='#page-top'>KSA Aarschot</a>
+      <ul>
+          <li><a href='#info'>Info</a></li>
+          <li><a href='#News'>News</a></li>
+          <li><a href='#braggel'>Braggel</a></li>
+          <li><a href='#Leeftijden'>Leeftijden</a></li>
+          <li><a href='#Contact'>Contact</a></li>
+      </ul>
     </nav>
   )
 }
