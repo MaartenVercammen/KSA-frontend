1d1c98b1d52ce8f631317e706c4dc98b36d929aa
emvee2000 <34810297+emvee2000@users.noreply.github.com>
2022-09-09 19:50:44 +0200

    base popup

--- src/components/body/leeftijdBox.tsx
+++ src/components/body/leeftijdBox.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import LeeftijdenPopup from "./leeftijdenPopup";
+import { useState }  from 'react';
 
 type Props = {
   name: string;
@@ -8,13 +10,25 @@ type Props = {
 };
 
 const LeeftijdBox: React.FC<Props> = ({ name, img, startAge, endAge }: Props) => {
+  const [modal, setModal] = useState<boolean>(false)
+
+  const handleModalSwitch = () => {
+    if(modal){
+      setModal(false);
+    }
+    else{
+      setModal(true);
+    }
+  }
+
   return (
-    <div className="leeftijdBox">
+    <div className="leeftijdBox" onClick={e => handleModalSwitch()}>
       <img src={img}/>
       <div className="info">
         <h4>{name}</h4>
         <p>{startAge}-{endAge}</p>
       </div>
+      {modal && <LeeftijdenPopup content={(<div>test</div>)} handleState={() => {handleModalSwitch()}}/>}
     </div>
   );
 };
