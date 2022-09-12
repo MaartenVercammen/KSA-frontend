4048d1fac88ce1a492b830188629e2f5039bf1f1
emvee2000 <34810297+emvee2000@users.noreply.github.com>
2022-09-12 11:00:51 +0200

    fix foto's

--- src/components/body/contactForm.tsx
+++ src/components/body/contactForm.tsx
@@ -5,13 +5,14 @@ const ContactFrom = () => {
     <div className='form-container'>
         <form>
 
-            <label htmlFor="name"> Name
-            <input type="text" id="name" name="name" placeholder="Your name.."/>
+            <label htmlFor="name"> Naam
+            <input type="text" id="name" name="name" placeholder="jan jansens"/>
             </label>
             <label htmlFor="email">Email
-            <input type="text" id="email" name="email" placeholder="Your email.."/>
+            <input type="text" id="email" name="email" placeholder="jan.jansens@email.be"/>
             </label>
-            <label htmlFor="subject">Subject</label>
+            <label htmlFor="onderwerp">Onderwerp</label>
+            <input type="text" id="onderwerp" name='onderwerp' placeholder='(onderwerp)'/>
             <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
             
             <input type="submit" value="Submit"/>
