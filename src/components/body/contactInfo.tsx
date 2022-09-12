cc844ff8fae2b89ac5c091cf1b67219b76ac8c70
emvee2000 <34810297+emvee2000@users.noreply.github.com>
2022-09-12 14:58:03 +0200

    footer

--- src/components/body/contactInfo.tsx
+++ src/components/body/contactInfo.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "./card";
 
-const ContactInfo = () => {
+const ContactInfo: React.FC = () => {
   return (
     <div className="contact-info">
-  
       <h3>Hoofdleiding</h3>
       <ul>
         <li>
-          <Card naam="Nele Hermans" nummer="0485681274" img="/images/rubskesLogo.png"/>
+          <Card
+            naam="Nele Hermans"
+            nummer="0485681274"
+            img="/images/rubskesLogo.png"
+          />
         </li>
         <li>
-          <Card naam="Kobe Holemens" nummer="0485681274" img="/images/rubskesLogo.png"/>
+          <Card
+            naam="Kobe Holemens"
+            nummer="0485681274"
+            img="/images/rubskesLogo.png"
+          />
         </li>
       </ul>
-      <p><h3>Mail:</h3> 
+      <h3>Mail:</h3>
+      <p>
         <a href="mailto:bonds@ksa-aarschot.be">Bonds@ksa-aarschot.be</a>
       </p>
     </div>
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer></footer>
-  )
-}
+    <footer>
+      <p>
+        &#169; 2015 KSA Aarschot -{" "}
+        <a href="mailto:info@ksa-aarschot.be">
+        &#9993; info@ksa-aarschot.be
+        </a>{" "}
+        - <a href="/login">&#9881; beheer</a>
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
