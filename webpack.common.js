20034a212763f09044505c154ebe9e2e686bacc3
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-09-12 18:30:16 +0200

    fix 404 reload

--- webpack.common.js
+++ webpack.common.js
@@ -15,6 +15,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist/build/'),
         filename: '[name].js',
+        publicPath: '/',
     },
     devServer: {
         historyApiFallback: { index: '/build/' },
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,5 +3,8 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 
 module.exports = merge(common,{
-    mode: 'development'
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+      }
 })
