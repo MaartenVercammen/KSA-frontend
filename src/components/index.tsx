0a0fe514d6c91081c434c8180a9c35f26fe7a953
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-09-13 13:04:17 +0200

    test backend

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Body from './body/body'
 import Footer from './footer/footer'
 import Header from './header/header'
 import NavBar from './header/navbar'
+import service from '../service/userservice'
+import { AxiosResponse } from 'axios'
 
 const Index = () => {
+
+  useEffect(() => {
+    getUsers()
+  
+  }, [])
+
+  const getUsers =async () => {
+    const res: AxiosResponse = await service.getUsers()
+    console.log(res.data)
+  }
+  
+
   return (
     <div className='page'>
         <Header/>
