1968c3f98687da30265c56dd46e48679bc2e87fa
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-09-13 10:02:41 +0200

    update to full green stats

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      <img src="/images/groepsfoto.jpg" className="headerImg" alt='groepsfoto bij de formatie'/>
+      <img src="/images/groepsfoto.webp" className="headerImg" alt='groepsfoto bij de formatie'/>
       <img src="/images/logo.png" className="logo" alt='logo van KSA aarschot'/>
     </header>
   )
