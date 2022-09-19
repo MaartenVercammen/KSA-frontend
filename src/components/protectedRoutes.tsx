9010151f699d26dc3a4ea521611131f82133346d
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-09-19 21:29:59 +0200

    add auth

--- src/components/protectedRoutes.tsx
+++ src/components/protectedRoutes.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { Navigate, Outlet  } from 'react-router-dom'
 import { tokenToString } from 'typescript';
-import UserService from '../service/userservice';
 import { Roles, user } from '../types/index'
 
 type Props = {
