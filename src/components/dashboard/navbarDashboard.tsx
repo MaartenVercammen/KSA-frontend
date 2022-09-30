634f0d6013e9984a4e9ce9d53a3ecc3a06716cc3
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-09-30 10:49:35 +0200

    retract navbar on click small view

--- src/components/dashboard/navbarDashboard.tsx
+++ src/components/dashboard/navbarDashboard.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
   setactivetab: (index: number) => void;
 };
 
 const NavbarDashboard = ({ setactivetab }: Props) => {
+  const [navdropdown, setnavdropdown] = useState<boolean>(false);
+
   const changetab = (index: number) => {
     setactivetab(index);
   };
@@ -16,13 +18,33 @@ const NavbarDashboard = ({ setactivetab }: Props) => {
         <label htmlFor="burger" className="hamburger">
           ☰
         </label>
-        <input type="checkbox" name="burger" id="burger" />
+        <input
+          type="checkbox"
+          name="burger"
+          id="burger"
+          checked={navdropdown}
+          onClick={(e) => setnavdropdown(true)}
+        />
         <ul>
           <li>
-            <a onClick={(e) => changetab(0)}>Braggels</a>
+            <a
+              onClick={(e) => {
+                changetab(0);
+                setnavdropdown(false);
+              }}
+            >
+              Braggels
+            </a>
           </li>
           <li>
-            <a onClick={(e) => changetab(1)}>Users</a>
+            <a
+              onClick={(e) => {
+                changetab(1);
+                setnavdropdown(false);
+              }}
+            >
+              Users
+            </a>
           </li>
         </ul>
       </nav>
--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashLink } from "react-router-hash-link";
 import "./header.css";
 
 const NavBar = () => {
+  const [checkboxstate, setcheckboxstate] = useState<boolean>(false);
   const scrollWithOffset = (el: any) => {
     const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
     const yOffset = -60;
@@ -19,25 +20,51 @@ const NavBar = () => {
       <label htmlFor="burger" className="hamburger">
         ☰
       </label>
-      <input type="checkbox" name="burger" id="burger" />
+      <input
+        type="checkbox"
+        name="burger"
+        id="burger"
+        onClick={(e) => setcheckboxstate(true)}
+        checked={checkboxstate}
+      />
       <ul>
         <li>
-          <HashLink smooth to="/#news" scroll={scrollWithOffset}>
+          <HashLink
+            smooth
+            to="/#news"
+            scroll={scrollWithOffset}
+            onClick={(e) => setcheckboxstate(false)}
+          >
             News
           </HashLink>
         </li>
         <li>
-          <HashLink smooth to="/#braggel" scroll={scrollWithOffset}>
+          <HashLink
+            smooth
+            to="/#braggel"
+            scroll={scrollWithOffset}
+            onClick={(e) => setcheckboxstate(false)}
+          >
             Braggel
           </HashLink>
         </li>
         <li>
-          <HashLink smooth to="/#leeftijden" scroll={scrollWithOffset}>
+          <HashLink
+            smooth
+            to="/#leeftijden"
+            scroll={scrollWithOffset}
+            onClick={(e) => setcheckboxstate(false)}
+          >
             Leeftijden
           </HashLink>
         </li>
         <li>
-          <HashLink smooth to="/#contact" scroll={scrollWithOffset}>
+          <HashLink
+            smooth
+            to="/#contact"
+            scroll={scrollWithOffset}
+            onClick={(e) => setcheckboxstate(false)}
+          >
             Contact
           </HashLink>
         </li>
