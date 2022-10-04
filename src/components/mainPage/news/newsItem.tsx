f55c2a6d4a4f4259d1eeea9d473f31f217e90441
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-10-04 09:15:59 +0200

    add p tag to news

--- src/components/mainPage/news/newsItem.tsx
+++ src/components/mainPage/news/newsItem.tsx
@@ -1,24 +1,26 @@
-import React from "react";
-import parse from "html-react-parser";
+import React from 'react';
+import parse from 'html-react-parser';
 
 type Props = {
-  title: string;
-  text: string;
-  date: string;
+    title: string;
+    text: string;
+    date: string;
 };
 
 const NewsItem: React.FC<Props> = ({ date, title, text }: Props) => {
-  return (
-    <div className="newsItem">
-      <h2>{title}</h2>
-      <div className="newsItemBody">
-        <div>{parse(text)}</div>
-      </div>
-      <p className="publishedOn">
-        <i>published on:{date}</i>
-      </p>
-    </div>
-  );
+    return (
+        <div className="newsItem">
+            <h2>{title}</h2>
+            <div className="newsItemBody">
+                <div>
+                    <p>{parse(text)}</p>
+                </div>
+            </div>
+            <p className="publishedOn">
+                <i>published on:{date}</i>
+            </p>
+        </div>
+    );
 };
 
 export default NewsItem;
