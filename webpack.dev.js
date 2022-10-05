3d336a9dc295f49b64829c657e97d4eed455aa3e
CrlH <10894454+CrlH@users.noreply.github.com>
2022-10-05 19:16:39 +0200

    [webpack] removing webpack-merge to allow more specific configurations

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,46 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common')
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = merge(common,{
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, './dist/dev/'),
-        filename: 'main.js',
-        publicPath: '/',
-    },
-    plugins: [
-        new Dotenv({
-            path: path.resolve(__dirname, './.env.dev') // Path to .env file (this is the default)
-        }),
+module.exports = {
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  mode: 'development',
+  output: {
+      path: path.resolve(__dirname, './dist/dev/'),
+      filename: 'main.js',
+      publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+    new Dotenv({
+        path: path.resolve(__dirname, './.env.dev') // Path to .env file (this is the default)
+    }),
+  ],
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader',  "sass-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader'],
+      },
     ],
-})
+  },
+}
