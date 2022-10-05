91aa1de9e40fda82744f400e9a74ec6a890dad8d
CrlH <10894454+CrlH@users.noreply.github.com>
2022-10-05 19:46:27 +0200

    [webpack] build optimizing

--- webpack.prod.js
+++ webpack.prod.js
@@ -20,6 +20,24 @@ module.exports = {
         path: './.env.production', // Path to .env file (this is the default)
     }),
   ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: (module) => {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
   module: {
     rules: [
       {
