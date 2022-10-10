8e53c20e3380061ef1f10e4c990e261391128874
CrlH <10894454+CrlH@users.noreply.github.com>
2022-10-10 12:06:13 +0200

    manual merge of changes on dev

--- src/components/mainPage/contact/socials.tsx
+++ src/components/mainPage/contact/socials.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+
+
 import Facebook from '../../../icons/facebook.svg';
 import Instagram from '../../../icons/instagram.svg';
+
+import styles from './socials.module.css';
+
 const Socials = () => {
 	return (
 		<div>
 			<h2>Volg ons op onze socials</h2>
 			<p>
-				<a href="https://www.facebook.com/KSA.Aarschot">
-					<Facebook className="fa fa-facebook" />
-				</a>
-				<a href="https://www.instagram.com/ksa_aarschot/">
-					<Instagram className="fa fa-instagram" />
-				</a>
+        <a href="https://www.facebook.com/KSA.Aarschot">
+          <Facebook className={`${styles.fa} ${styles['fa-facebook']}`} />
+        </a>
+        <a href="https://www.instagram.com/ksa_aarschot/">
+          <Instagram className={`${styles.fa} ${styles['fa-instagram']}`} />
+        </a>
 			</p>
 		</div>
 	);
