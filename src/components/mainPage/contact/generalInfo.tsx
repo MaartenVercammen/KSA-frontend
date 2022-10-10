d32a11dc87d024c9214b640bf2f61d6fb0566b93
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2022-10-10 10:06:52 +0200

    update layout

--- src/components/mainPage/contact/generalInfo.tsx
+++ src/components/mainPage/contact/generalInfo.tsx
@@ -1,65 +1,23 @@
 import React from 'react';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-import Facebook from '../../../icons/facebook.svg';
-import Instagram from '../../../icons/instagram.svg';
+import Socials from './socials';
 
 function GeneralInfo() {
-  const key: string = process.env.GOOGLE_KEY === undefined ? '' : process.env.GOOGLE_KEY;
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: key,
-  });
-
-  return (
-    <div className="generalinfo">
-      <div>
-        <h2>KSA aarschot</h2>
-        <p>
-          Elke zaterdag vanaf 14.00u, voor jongens en meisjes vanaf de 3de
-          kleuterklas.
-        </p>
-      </div>
-      <div>
-        <h2>Vragen? mail ons</h2>
-        <p>
-          <a href="mailto:bonds@ksa-aarschot.be">Bonds@ksa-aarschot.be</a>
-        </p>
-        <div>
-          <h2>Volg ons op onze socials</h2>
-          <p>
-            <a href="https://www.facebook.com/KSA.Aarschot">
-              <Facebook className="fa fa-facebook" />
-            </a>
-            <a href="https://www.instagram.com/ksa_aarschot/">
-              <Instagram className="fa fa-instagram" />
-            </a>
-          </p>
-        </div>
-      </div>
-      <div>
-        <h3>Locatie:</h3>
-        <p>
-          <a href="https://www.google.com/maps/place/Ksa+Aarschot/@50.985566,4.8416534,17z/data=!3m1!4b1!4m5!3m4!1s0x47c143501a3e8e41:0x3b8001192ca15fa1!8m2!3d50.9855579!4d4.8461356">
-            Bekaflaan 65 3200 Aarschot
-          </a>
-        </p>
-
-        {isLoaded && (
-          <div className="map">
-            <GoogleMap
-              zoom={15}
-              center={{ lat: 50.985687650951384, lng: 4.84617028489699 }}
-              mapContainerClassName="map-container"
-            >
-              <Marker
-                position={{ lat: 50.985687650951384, lng: 4.84617028489699 }}
-              />
-            </GoogleMap>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="generalinfo">
+			<div>
+				<h2>KSA aarschot</h2>
+				<p>Elke zaterdag vanaf 14.00u, voor jongens en meisjes vanaf de 3de kleuterklas.</p>
+			</div>
+			<div>
+				<h2>Vragen? mail ons</h2>
+				<p>
+					<a href="mailto:bonds@ksa-aarschot.be">Bonds@ksa-aarschot.be</a>
+				</p>
+			</div>
+			<Socials />
+		</div>
+	);
 }
 
 export default GeneralInfo;
--- src/components/mainPage/news/newsItem.tsx
+++ src/components/mainPage/news/newsItem.tsx
@@ -2,35 +2,33 @@ import React from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 type Props = {
-  title: string;
-  text: string;
-  date: string;
+	title: string;
+	text: string;
+	date: string;
 };
 
 function NewsItem({ date, title, text }: Props) {
-  return (
-    <div className="newsItem">
-      <h2>{title}</h2>
-      <div className="newsItemBody">
-        <div>
-          <MDEditor.Markdown
-            source={text}
-            style={{
-              whiteSpace: 'pre-wrap',
-              backgroundColor: '#f4901d',
-              color: 'white',
-            }}
-          />
-        </div>
-      </div>
-      <p className="publishedOn">
-        <i>
-          published on:
-          {date}
-        </i>
-      </p>
-    </div>
-  );
+	return (
+		<div className="newsItem">
+			<h2>{title}</h2>
+			<div className="newsItemBody">
+				<MDEditor.Markdown
+					source={text}
+					style={{
+						whiteSpace: 'pre-wrap',
+						backgroundColor: '#f4901d',
+						color: 'white'
+					}}
+				/>
+			</div>
+			<p className="publishedOn">
+				<i>
+					published on:
+					{date}
+				</i>
+			</p>
+		</div>
+	);
 }
 
 export default NewsItem;
