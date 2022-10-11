215a3f1cbff0b1b8a893c10f93aa59fd8bf9fccb
CrlH <10894454+CrlH@users.noreply.github.com>
2022-10-11 09:39:33 +0200

    let personal pictures be handled by server

--- src/App.tsx
+++ src/App.tsx
@@ -16,32 +16,6 @@ import jongknapenLogo from './images/jongknapenLogo.webp';
 import knapenLogo from './images/knapenLogo.webp';
 import jonghernieuwersLogo from './images/jonghernieuwersLogo.webp';
 import hernieuwersLogo from './images/hernieuwersLogo.webp';
-import annSofie from './images/ann-sofie.webp';
-import anneSofie from './images/anne-sofie.webp';
-import charlotte from './images/charlotte.webp';
-import daan from './images/daan.webp';
-import demi from './images/demi.webp';
-import finn from './images/finn.webp';
-import floor from './images/floor.webp';
-import giel from './images/giel.webp';
-import joeri from './images/joeri.webp';
-import joke from './images/joke.webp';
-import juul from './images/juul.webp';
-import kato from './images/kato.webp';
-import kobe from './images/kobe.webp';
-import leon from './images/leon.webp';
-import lore from './images/lore.webp';
-import luna from './images/luna.webp';
-import margot from './images/margot.webp';
-import marthe from './images/marthe.webp';
-import nele from './images/nele.webp';
-import roos from './images/roos.webp';
-import rosemarie from './images/rosemarie.webp';
-import ruben from './images/ruben.webp';
-import sean from './images/sean.webp';
-import seppe from './images/seppe.webp';
-import tjen from './images/tjen.webp';
-import tuur from './images/tuur.webp';
 
 function App() {
   const setToken = (userToken: object) => {
@@ -52,7 +26,9 @@ function App() {
     <>
       <main>
         <Routes>
-          <Route path="/*" element={<Index />} />
+          <Route path="/" element={<Index />} />
+          <Route path="/login" element={<Login setToken={setToken} />} />
+          <Route path="/logout" element={<Logout />} />
           <Route
             path="/leiders/Rubskes"
             element={(
@@ -63,35 +39,35 @@ function App() {
                     voornaam: 'Floor',
                     achternaam: 'Van Casteren',
                     number: '0499 20 10 60',
-                    img: floor,
+                    img: '/images/floor.webp',
                   },
                   {
                     voornaam: 'Rosemarie',
                     achternaam: 'Mas',
                     number: '0491 19 26 20',
-                    img: rosemarie,
+                    img: '/images/rosemarie.webp',
                   },
                   {
                     voornaam: 'Giel',
                     achternaam: 'De Bont',
                     number: '0494 46 55 14',
-                    img: giel,
+                    img: '/images/giel.webp',
                   },
                   {
                     voornaam: 'Charlotte',
                     achternaam: 'Morlion',
                     number: '0493 20 95 72',
-                    img: charlotte,
+                    img: '/images/charlotte.webp',
                   },
                   {
                     voornaam: 'Demi',
                     achternaam: 'Jonckheere',
                     number: '0483 45 15 80',
-                    img: demi,
+                    img: '/images/demi.webp',
                   },
                 ]}
               />
-                          )}
+     )}
           />
           <Route
             path="/leiders/Leeuwkes"
@@ -103,37 +79,37 @@ function App() {
                     voornaam: 'Nele',
                     achternaam: 'Hermans',
                     number: '0472 06 42 54 ',
-                    img: nele,
+                    img: '/images/nele.webp',
                   },
                   {
                     voornaam: 'Margot',
                     achternaam: ' Somers',
                     number: '0470 82 72 58',
-                    img: margot,
+                    img: '/images/margot.webp',
                   },
                   {
                     voornaam: 'Kato',
                     achternaam: 'Willems',
                     number: '0498 06 93 51',
-                    img: kato,
+                    img: '/images/kato.webp',
                   },
                   {
                     voornaam: 'Seppe',
                     achternaam: 'Bellekens',
                     number: '0478 52 48 75',
-                    img: seppe,
+                    img: '/images/seppe.webp',
                   },
                   {
                     voornaam: 'Joeri',
                     achternaam: 'Perdieus',
                     number: '0468 20 50 08 ',
-                    img: joeri,
+                    img: '/images/joeri.webp',
                   },
                   {
                     voornaam: 'Marthe',
                     achternaam: 'Seldeslachts',
                     number: '0472 63 42 63',
-                    img: marthe,
+                    img: '/images/marthe.webp',
                   },
                 ]}
               />
@@ -149,41 +125,41 @@ function App() {
                     voornaam: 'Joke',
                     achternaam: 'Hermans',
                     number: '0491 20 88 08 ',
-                    img: joke,
+                    img: '/images/joke.webp',
                   },
                   {
                     voornaam: 'Lore',
                     achternaam: 'Willems',
                     number: '0488 33 72 33',
-                    img: lore,
+                    img: '/images/lore.webp',
                   },
                   {
                     voornaam: 'Tuur',
                     achternaam: 'Willems',
                     number: '0468 33 61 46 ',
-                    img: tuur,
+                    img: '/images/tuur.webp',
                   },
                   {
                     voornaam: 'Leon',
                     achternaam: 'Decoster',
                     number: '0468 27 29 10',
-                    img: leon,
+                    img: '/images/leon.webp',
                   },
                   {
                     voornaam: 'Sean',
                     achternaam: 'Oliviers',
                     number: '0486 11 44 03',
-                    img: sean,
+                    img: '/images/sean.webp',
                   },
                   {
                     voornaam: 'Anne-Sofie',
                     achternaam: 'Van Bael',
                     number: '0493 74 95 88',
-                    img: anneSofie,
+                    img: '/images/anne-sofie.webp',
                   },
                 ]}
               />
-                          )}
+     )}
           />
           <Route
             path="/leiders/Knapen"
@@ -195,23 +171,23 @@ function App() {
                     voornaam: 'Ann-Sofie',
                     achternaam: 'Milis',
                     number: '0497 22 68 47',
-                    img: annSofie,
+                    img: '/images/ann-sofie.webp',
                   },
                   {
                     voornaam: 'Ruben',
                     achternaam: 'Pauwels',
                     number: '0468 26 45 59',
-                    img: ruben,
+                    img: '/images/ruben.webp',
                   },
                   {
                     voornaam: 'Roos',
                     achternaam: 'Debeys',
                     number: '0479 30 70 17',
-                    img: roos,
+                    img: '/images/roos.webp',
                   },
                 ]}
               />
-                          )}
+     )}
           />
           <Route
             path="/leiders/Jonghernieuwers"
@@ -223,23 +199,23 @@ function App() {
                     voornaam: 'Daan',
                     achternaam: 'Van Noten',
                     number: '0496 39 51 75',
-                    img: daan,
+                    img: '/images/daan.webp',
                   },
                   {
                     voornaam: 'Finn',
                     achternaam: 'Bogaerts',
                     number: '0479 01 98 66',
-                    img: finn,
+                    img: '/images/finn.webp',
                   },
                   {
                     voornaam: 'Juul',
                     achternaam: 'Schellens',
                     number: '0492 08 35 60',
-                    img: juul,
+                    img: '/images/juul.webp',
                   },
                 ]}
               />
-                          )}
+     )}
           />
           <Route
             path="/leiders/Hernieuwers"
@@ -251,25 +227,24 @@ function App() {
                     voornaam: 'Kobe',
                     achternaam: 'Holemans',
                     number: '0499 30 40 12',
-                    img: kobe,
+                    img: '/images/kobe.webp',
                   },
                   {
                     voornaam: 'Tjen',
                     achternaam: 'Ooms',
                     number: '0470 39 44 72',
-                    img: tjen,
+                    img: '/images/tjen.webp',
                   },
                   {
                     voornaam: 'Luna',
                     achternaam: 'Vervloessem',
                     number: '0473 98 97 90',
-                    img: luna,
+                    img: '/images/luna.webp',
                   },
                 ]}
               />
-                          )}
+     )}
           />
-          <Route path="/login" element={<Login setToken={setToken} />} />
           <Route
             element={(
               <ProtectedRoutes
@@ -280,7 +255,6 @@ function App() {
           >
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
-          <Route path="logout" element={<Logout />} />
         </Routes>
       </main>
       <Footer />
