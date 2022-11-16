606655643d012198773b71cca5ac51327dd8f55f
CrlH <10894454+CrlH@users.noreply.github.com>
2022-11-16 08:35:21 +0100

    [optimization] remove interactive Google Map

--- src/scenes/mainPage/contact/locatie.tsx
+++ src/scenes/mainPage/contact/locatie.tsx
@@ -1,29 +1,14 @@
 import React from 'react';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 import styles from './locatie.module.css';
 
 function Locatie() {
-  const key: string = process.env.GOOGLE_KEY === undefined ? '' : process.env.GOOGLE_KEY;
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: key,
-  });
   return (
     <div className={styles.container}>
       <h3>Waar kan je ons vinden?</h3>
       <a href="https://www.google.com/maps/place/Ksa+Aarschot/@50.985566,4.8416534,17z/data=!3m1!4b1!4m5!3m4!1s0x47c143501a3e8e41:0x3b8001192ca15fa1!8m2!3d50.9855579!4d4.8461356">
         Bekaflaan 65 3200 Aarschot
       </a>
-
-      {isLoaded && (
-      <GoogleMap
-        zoom={15}
-        center={{ lat: 50.985687650951384, lng: 4.84617028489699 }}
-        mapContainerClassName={styles.map}
-      >
-        <Marker position={{ lat: 50.985687650951384, lng: 4.84617028489699 }} />
-      </GoogleMap>
-      )}
     </div>
   );
 }
